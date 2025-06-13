acda2b7c96bce3bd1b3a9b4be077938a79d83890
Minseop Choi (Wade) <165863141+minsubb13@users.noreply.github.com>
2025-06-13 16:29:11 +0900

    code-health (#23)
    Refactoring the code for code health
    
    issue: None

--- elliptic-curves-rs/src/protocols/tests/ecdh_secp256k1.rs
+++ elliptic-curves-rs/src/protocols/tests/ecdh_secp256k1.rs
@@ -8,6 +8,10 @@ fn test_ecdh_secp256k1() {
     let (bob_private_key, bob_public_key) = 
         Ecdh::<Secp256k1Curve>::generate_keypair();
 
+    // Alice's and Bob's keys should be different
+    assert_ne!(alice_public_key, bob_public_key);
+    assert_ne!(alice_private_key, bob_private_key);
+
     // Ecdh 구조체에서 어떤 구체적인 타입을 사용할지 :: 를 이용해 명시적으로 알려줌
     let shared_alice = Ecdh::<Secp256k1Curve>::compute_shared_secret(
                                 &alice_private_key, &bob_public_key);
--- elliptic-curves-rs/src/protocols/tests/ecdsa_secp256k1.rs
+++ elliptic-curves-rs/src/protocols/tests/ecdsa_secp256k1.rs
@@ -8,22 +8,30 @@ use sha2::{Sha256, Digest};
 
 #[test]
 fn test_ecdsa_secp256k1() {
-    let (alice_private_key, alice_public_key) = 
+    let (alice_private_key, alice_public_key) =
         Ecdh::<Secp256k1Curve>::generate_keypair();
-    let (bob_private_key, bob_public_key) = 
+    let (bob_private_key, bob_public_key) =
         Ecdh::<Secp256k1Curve>::generate_keypair();
-    assert_ne!(alice_private_key, bob_private_key);
+    
+    // Alice's and Bob's keys should be different
     assert_ne!(alice_public_key, bob_public_key);
+    assert_ne!(alice_private_key, bob_private_key);
 
     let digest = Sha256::digest(b"hello world");
     let z: FrSecp256k1 = FrSecp256k1::from_be_bytes_mod_order(&digest);
-
     let wrong_digest = Sha256::digest(b"hello there");
     let wrong_z = FrSecp256k1::from_be_bytes_mod_order(&wrong_digest);
 
-    let signature = Ecdsa::<Secp256k1Curve>::signing_message(&alice_private_key, z);
+    let signature =
+        Ecdsa::<Secp256k1Curve>::signing_message(&alice_private_key, z);
     
-    assert_eq!(true, Ecdsa::<Secp256k1Curve>::verifying_message(&alice_public_key, z, &signature));
-    assert_eq!(false, Ecdsa::<Secp256k1Curve>::verifying_message(&alice_public_key, wrong_z, &signature));
-    assert_eq!(false, Ecdsa::<Secp256k1Curve>::verifying_message(&bob_public_key, z, &signature));
+    assert!(Ecdsa::<Secp256k1Curve>::verifying_message(
+        &alice_public_key, z, &signature
+    ));
+    assert!(!Ecdsa::<Secp256k1Curve>::verifying_message(
+        &alice_public_key, wrong_z, &signature
+    ));
+    assert!(!Ecdsa::<Secp256k1Curve>::verifying_message(
+        &bob_public_key, z, &signature
+    ));
 }
